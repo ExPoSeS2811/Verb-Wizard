e10f596be6b08d35f9adca60863b43d010002576
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-17 17:13:39 +0300

    initial commit + deleted Main.storyboard

---
+++ VerbWizard/VerbWizard/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  VerbWizard
+//
+//  Created by MacBook Pro 13 2019 on 4/17/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let navigationController = UINavigationController()
+            navigationController.viewControllers = [ViewController()]
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
+
